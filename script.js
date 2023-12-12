62d31fbb63713c26484df1da2b47a9e38aa82476
ANILKUMARRegidi <129140166+ANILKUMARRegidi@users.noreply.github.com>
2023-12-12 20:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+ function showPopup() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'block';
+  }
+
+  // Function to hide the popup
+  function hidePopup() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+  }
